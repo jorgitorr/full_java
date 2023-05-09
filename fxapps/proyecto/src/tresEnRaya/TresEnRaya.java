55182d9629f83a9ab5e5c94320d2759eb5e85ba0
jorgitorr <jorgearceruiz97@gmail.com>
2023-05-10 00:15:02 +0200

    n

--- fxapps/proyecto/src/tresEnRaya/TresEnRaya.java
+++ fxapps/proyecto/src/tresEnRaya/TresEnRaya.java
@@ -5,98 +5,30 @@ package tresEnRaya;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 
-import java.util.Scanner;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  *
  * @author jorgearru
  */
-public class TresEnRaya {
-    private char[]tablero;
-    private int partidas;
-    private char turno;
-    private boolean ganador;
+public class TresEnRaya extends Application {
 
-    public TresEnRaya() {
-        this.tablero = new char[9];
-        this.partidas = 9;
-        this.turno = 'X';
-        this.ganador = false;
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("tresEnRayafx.fxml"));
+        primaryStage.setTitle("Tres En Raya");
+        primaryStage.setScene(new Scene(root, 450, 400));
+        primaryStage.show();
     }
 
-    public void getTablero(){
-        String line = "";
-        for (int i = 0; i < tablero.length; i++) {
-            if (tablero[i] == 0) {
-                line += " ";
-            } else {
-                line += tablero[i];
-            }
-            if (i % 3 == 2) {
-                System.out.println(String.format("| %c | %c | %c |", line.charAt(0), line.charAt(1), line.charAt(2)));
-                System.out.println("+---+---+---+");
-                line = "";
-            }
-        }
-    }
-
-    
-    private boolean checkPos(int pos){
-        boolean ocupado = false;
-        if(pos<0 || pos>=9)
-            ocupado = true;
-        else if(tablero[pos]=='X' || tablero[pos]=='0')
-            ocupado = true;
-        return ocupado;
-    }
-    
-    public void checkGanador(){
-        if(tablero[0]==turno && tablero[1]==turno && tablero[2]==turno)
-            ganador = true;
-        if(tablero[3]==turno && tablero[4]==turno && tablero[5]==turno)
-            ganador = true;
-        if(tablero[6]==turno && tablero[7]==turno && tablero[8]==turno)
-            ganador = true;
-        if(tablero[0]==turno && tablero[4]==turno && tablero[8]==turno)
-            ganador = true;
-        if(tablero[2]==turno && tablero[4]==turno && tablero[6]==turno)
-            ganador = true;     
-    }
-    
-
-    public void juega(){
-        Scanner sc = new Scanner(System.in);
-        int pos;
-
-        do{
-            do{
-                System.out.println("Introduce la posicion de " + turno);
-                pos = sc.nextInt()-1;
-            }while(checkPos(pos));
-            
-            tablero[pos] = turno;
-            
-            getTablero();
-            checkGanador();
-            
-            if(ganador)
-                break;
-            else if(turno == 'X')
-                turno = '0';
-            else
-                turno = 'X';
-            partidas--;
-            
-        }while(partidas>0 && !ganador);
-        
-        if(ganador)
-            System.out.println("El ganador es " + turno);
-        else
-            System.out.println("Empate");
-    }
     public static void main(String[] args) {
-        TresEnRaya partida = new TresEnRaya();
-        partida.juega();
+        launch(args);
     }
+
     
 }
